a005b8e76fe6f5c0ad0df989405c08ca3eda3271
Vitaliy-1915 <74369854+Vitaliy-1915@users.noreply.github.com>
2021-11-12 23:35:39 +0200

    добавил запись в localStorage

--- src/App.js
+++ src/App.js
@@ -17,6 +17,21 @@ export class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   formSubmit = value => {
     const contact = {
       id: unId(),
